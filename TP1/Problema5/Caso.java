5d258b684038ea1bdc671ddd13aa8896508338f5
nachoperna <ignacioperna@gmail.com>
2026-06-07 12:30:49 -0300

    inicializacion de casos Problema5

---
+++ TP1/Problema5/Caso.java
@@ -0,0 +1,12 @@
+public class Caso {
+      private Character[][] mapa;
+      private int F;
+      private int C;
+
+      public Caso(Character[][] mapa, int F, int C){
+            this.mapa = mapa;
+            this.F = F;
+            this.C = C;
+      }
+      
+}
---
+++ TP1/Problema5/Main.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+      public static void main(String[] args) {
+            List<Caso> casos = new ArrayList<>();
+            initCasos(casos);
+      }
+
+      public static void initCasos(List<Caso> casos) {
+            casos.add(new Caso(new Character[][]{
+                  {'E', '.', '.'},
+                  {'.', '.', '.'},
+                  {'.', '.', 'S'}
+            }, 3, 3));
+            casos.add(new Caso(new Character[][]{
+                  {'E', '#', '.'},
+                  {'.', '#', '.'},
+                  {'.', '#', 'S'}
+            }, 3, 3));
+            casos.add(new Caso(new Character[][]{
+                  {'E', '.', 'S'},
+                  {'.', '.', '.'},
+                  {'.', '.', 'S'}
+            }, 3, 3));
+            casos.add(new Caso(new Character[][]{
+                  {'S', '.', 'b', '#' , 'b'},
+                  {'#', '#', '#', '#' , 'a'},
+                  {'.', '.', 'E', '#' , '#'},
+                  {'c', '#', '#', '.' , 'c'},
+                  {'#', 'a', '.', '.' , '.'},
+            }, 5, 5));
+      }
+}
